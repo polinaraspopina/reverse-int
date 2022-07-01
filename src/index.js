4d0b29a4743cebac3f129f4eb44f620d2e2bd1b1
Polina Raspopina <polinaraspopina2512@gmail.com>
2022-07-01 13:51:27 +0700

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  let number = n.toString();
+  let reversedNumber = '';
+
+  if (number[0] === '-') { number = number.slice(1); }
+
+  for (let i = 0; i < number.length; i++) {
+    reversedNumber = `${number[i]}${reversedNumber}`;
+  }
+
+  return Number(reversedNumber);
 }
